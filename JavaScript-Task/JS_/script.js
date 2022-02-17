0ad827ea2aaaa226a4bc431b0f07bc25cfbcd1b3
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-17 22:56:26 +0400

    Var, Let, Const Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,26 +1,57 @@
-"use strict"
+// "use strict"
 
-// alert("salam dunya");
+// // alert("salam dunya");
 
 
-// window.alert("salamlar her kese");
-// document.write("salamlar");
+// // window.alert("salamlar her kese");
+// // document.write("salamlar");
 
-// document.write("salamlar");
-// console.log("burada sehv var");
-// console.log("5*5");
-// var x = 5;
-// var y = 4;
-// var c = x * y;
+// // document.write("salamlar");
+// // console.log("burada sehv var");
+// // console.log("5*5");
+// // var x = 5;
+// // var y = 4;
+// // var c = x * y;
+
+// // var ad = "Qurban";
+// // document.write("ad");
+
+
+// // var say = "5";
+// // document.write(say + 10);
 
 // var ad = "Qurban";
-// document.write("ad");
+// var yas = 28;
+// document.write("istifadeci adi:  " + ad + "<br>");
+// // // // document.write("istifadeci yasi:  " + yas)
 
+var age;
+console.log(age);
+age = 20;
+console.log(age);
 
-// var say = "5";
-// document.write(say + 10);
+var fullname = "Qurban Qurbanzade";
+console.log(fullname);
 
-var ad = "Qurban";
-var yas = 28;
-document.write("istifadeci adi:  " + ad + "<br>");
-document.write("istifadeci yasi:  " + yas);
+fullname = "Kenan Qurbanzade";
+console.log(fullname);
+
+// Deyisen tanima qaydalari
+// deyisen adlari reqem ile baslaya bilmez
+var yas1;
+var _yas2;
+var $yas3;
+
+// Element adlari ile baslaya bilmez
+// adlar birden cox olarsa
+
+var ad_soyad = "Qurban Qurbanzade";
+var adSoyad = "Qurban Qurbanzade";
+
+// var, let, const
+
+let city = "Beylagan";
+console.log(city);
+
+const email = "abc@gmail.com";
+console.log(email);
