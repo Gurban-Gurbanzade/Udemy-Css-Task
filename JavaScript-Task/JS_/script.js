66ae51d1e80d58c855375a12eaf9cd80ac61ca1e
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-17 22:05:24 +0400

    Dəyişənlər Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -5,3 +5,22 @@
 
 // window.alert("salamlar her kese");
 // document.write("salamlar");
+
+// document.write("salamlar");
+// console.log("burada sehv var");
+// console.log("5*5");
+// var x = 5;
+// var y = 4;
+// var c = x * y;
+
+// var ad = "Qurban";
+// document.write("ad");
+
+
+// var say = "5";
+// document.write(say + 10);
+
+var ad = "Qurban";
+var yas = 28;
+document.write("istifadeci adi:  " + ad + "<br>");
+document.write("istifadeci yasi:  " + yas);
