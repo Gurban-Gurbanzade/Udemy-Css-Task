2f4fbb6b266d4f32e134f2924b399164c99ad618
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-20 21:28:29 +0400

    Number Task

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,16 +1,30 @@
-let num = 10.37641;
-val = num.toPrecision(1);
-val = num.toFixed(2);
-val = Math.PI
-val = Math.round(2.4);
-val = Math.round(2.6);
-val = Math.ceil(2.3);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.pow(2, 3);
-val = Math.abs(-100);
-val = Math.min(2, 5, 7, 4, 9);
-val = Math.max(2, 45, 3, 23);
-val = Math.floor(Math.random() * 20 + 1);
+// let num = 10.37641;
+// val = num.toPrecision(1);
+// val = num.toFixed(2);
+// val = Math.PI
+// val = Math.round(2.4);
+// val = Math.round(2.6);
+// val = Math.ceil(2.3);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.pow(2, 3);
+// val = Math.abs(-100);
+// val = Math.min(2, 5, 7, 4, 9);
+// val = Math.max(2, 45, 3, 23);
+// val = Math.floor(Math.random() * 20 + 1);
 
-console.log(val);
+// console.log(val);
+
+let num = 15.123456789;
+
+val = num.toPrecision(3)
+val = num.toFixed(5)
+val = num.toFixed();
+val = Math.floor(num);
+val = Math.ceil(num);
+val = Math.min(1, 2, 10, 56, 20);
+val = Math.max(1, 2, 10, 56, 20);
+val = Math.random() * 40;
+
+
+console.log(val)
