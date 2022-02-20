686684ce1a90b4e0e54c68e239979c586d963821
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-20 22:13:10 +0400

    String Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,30 +1,38 @@
-// let num = 10.37641;
-// val = num.toPrecision(1);
-// val = num.toFixed(2);
-// val = Math.PI
-// val = Math.round(2.4);
-// val = Math.round(2.6);
-// val = Math.ceil(2.3);
-// val = Math.floor(2.8);
-// val = Math.sqrt(64);
-// val = Math.pow(2, 3);
-// val = Math.abs(-100);
-// val = Math.min(2, 5, 7, 4, 9);
-// val = Math.max(2, 45, 3, 23);
-// val = Math.floor(Math.random() * 20 + 1);
+const name = 'kenan';
+const surname = 'qurbanzade';
+const age = 35;
+let hobby = 'kino, kitab, idman, yazi'
+let val;
 
-// console.log(val);
+val = name + " " + surname;
+val = name;
+val += surname;
+val = 'menim adim ' + name + ' soyadim ' + surname + ' ve yasim ' + age;
 
-let num = 15.123456789;
+val = name.concat(' ', surname);
 
-val = num.toPrecision(3)
-val = num.toFixed(5)
-val = num.toFixed();
-val = Math.floor(num);
-val = Math.ceil(num);
-val = Math.min(1, 2, 10, 56, 20);
-val = Math.max(1, 2, 10, 56, 20);
-val = Math.random() * 40;
+//val = val.length;
+
+val = val.toUpperCase();
+val = val.toLowerCase();
+
+//val = val[2];
+
+//val = val.indexOf('e');
+
+//val = val.substring(0, 5);
+
+//val = val.slice(6);
+
+val = hobby.split(',');
 
 
-console.log(val)
+
+
+
+
+
+
+
+console.log(val);
+console.log(typeof val);
