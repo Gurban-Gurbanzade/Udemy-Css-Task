6067e650db25cfafbad25ea2832453f155d5ed03
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-20 15:52:01 +0400

    Dəyişənlər Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,8 +1,24 @@
-//alert("salam");
-console.log("salam");
-console.log(1234);
-console.log(true);
-console.log([1, 2, 3, 4]);
-console.error("xeta var");
-console.warn("diqqetli ol");
-console.clear()
+let age;
+age = 20;
+console.log(age);
+
+let fullname = "qurban qurbanzade";
+console.log(fullname);
+
+fullname = "kenan qurbanzade";
+console.log(fullname);
+
+let ad_soyad = "kenan qurbanzade";
+console.log(fullname);
+
+let firstname = "qurban";
+let Firstname = "kenan";
+
+console.log(firstname);
+console.log(Firstname);
+
+const email = "abc@gmail.com";
+console.log(email);
+
+//email = "bcd@gmail.com";
+//console.log(email);
