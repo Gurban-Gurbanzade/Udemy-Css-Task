002856e59e00d5986c13aaf2dc6a583aae379f36
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-20 22:52:43 +0400

    Template Literals Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,38 +1,49 @@
-const name = 'kenan';
-const surname = 'qurbanzade';
-const age = 35;
-let hobby = 'kino, kitab, idman, yazi'
+// const name = 'kenan';
+// const surname = 'qurbanzade';
+// const age = 35;
+// let hobby = 'kino, kitab, idman, yazi'
+// let val;
+
+// val = name + " " + surname;
+// val = name;
+// val += surname;
+// val = 'menim adim ' + name + ' soyadim ' + surname + ' ve yasim ' + age;
+
+// val = name.concat(' ', surname);
+
+// //val = val.length;
+
+// val = val.toUpperCase();
+// val = val.toLowerCase();
+
+// //val = val[2];
+
+// //val = val.indexOf('e');
+
+// //val = val.substring(0, 5);
+
+// //val = val.slice(6);
+
+// val = hobby.split(',');
+
+// console.log(val);
+// console.log(typeof val);
+
+let sentence = "men sabah derse getmeyeceyem"
+
+//sentence = sentence.length;
+//sentence = sentence.split(' ');
+sentence = sentence.toLowerCase();
+sentence = sentence.trim();
+sentence = sentence.replace('s', ' ');
+
+console.log(sentence);
+
+const name = 'kenan qurbanzade';
+const city = 'baku';
+const birthday = 1993;
 let val;
 
-val = name + " " + surname;
-val = name;
-val += surname;
-val = 'menim adim ' + name + ' soyadim ' + surname + ' ve yasim ' + age;
-
-val = name.concat(' ', surname);
-
-//val = val.length;
-
-val = val.toUpperCase();
-val = val.toLowerCase();
-
-//val = val[2];
-
-//val = val.indexOf('e');
-
-//val = val.substring(0, 5);
-
-//val = val.slice(6);
-
-val = hobby.split(',');
-
-
-
-
-
-
-
-
+val = `my name is ${name} yasiyiram ${city} yasim ${2022-birthday}`
 
 console.log(val);
-console.log(typeof val);
