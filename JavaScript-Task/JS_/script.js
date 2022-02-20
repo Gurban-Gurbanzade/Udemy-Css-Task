9b11d5a6be35bca91cc5fcba17d86187eb5df5a5
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-20 21:07:37 +0400

    Number Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,25 +1,16 @@
- let d = new Date();
- let birthday = new Date(1993, 9, 7);
+let num = 10.37641;
+val = num.toPrecision(1);
+val = num.toFixed(2);
+val = Math.PI
+val = Math.round(2.4);
+val = Math.round(2.6);
+val = Math.ceil(2.3);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.pow(2, 3);
+val = Math.abs(-100);
+val = Math.min(2, 5, 7, 4, 9);
+val = Math.max(2, 45, 3, 23);
+val = Math.floor(Math.random() * 20 + 1);
 
- // set methods
- d.setFullYear(2022);
- d.setMonth(5);
- d.setDate(2);
- d.setHours(12);
-
-
-
- // get methods
- console.log(d.getDate());
- console.log(d.getDay());
- console.log(d.getFullYear());
- console.log(d.getHours());
- console.log(d.getMonth());
- console.log(d.getSeconds());
-
-
- console.log(d);
- console.log(typeof d);
-
- console.log(d.getDate() - birthday.getDate());
- console.log(d.getFullYear() - birthday.getFullYear());
+console.log(val);
