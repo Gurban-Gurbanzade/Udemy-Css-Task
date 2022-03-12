c28ca3f46c2a068ffb2d0e8f5229f78fa07e943b
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-12 23:26:49 +0400

    Dəyişənlərin tip dəyişimi Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,16 +1,42 @@
-console.log("salam");
-console.log(1234);
-console.log(true);
-console.log([1, 2, 3]);
-console.error("sehv oldu");
-console.warn("xeberdarliq");
+// Deyishenler tip deyishimi
+// let x = Number("5");
+// let y = Number("10");
 
-// rey bildirmek
+// let z = x + y;
+let z;
+//number to string
 
-/*
-dkjflksadf
-dskf;dkjflksadflkfjl;
-dhflksd
-*/
+z = String(20);
 
-console.clear();
+// bool to string
+
+z = String(true);
+
+// date to string
+z = String(new Date().getFullYear());
+
+//array to string
+
+z = String([1, 2, 3]);
+
+//toString
+z = (10).toString();
+z = true.toString();
+
+//string to number
+
+z = Number("10");
+z = Number(true);
+z = Number(false);
+z = Number(null);
+z = Number("a");
+z = Number([1, 2, 3]);
+
+//parsInt parsFloat
+z = parseInt("10");
+z = parseInt("10.5");
+z = parseFloat("10.5");
+
+console.log(z);
+console.log(typeof z);
+//console.log(z.length);
