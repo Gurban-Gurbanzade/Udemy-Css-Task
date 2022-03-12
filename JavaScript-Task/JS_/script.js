156d3dc71a0ca87035abd02065c930fd156c7c67
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-12 22:28:46 +0400

    Console.log Example

--- JavaScript-Task/JS_/script.js
+++ JavaScript-Task/JS_/script.js
@@ -1,64 +1,16 @@
-// Arrays
-let names = ['qurban', 'kenan', 'eli'];
-let years = [2020, 1993, 2022, 1995];
-let mix = ['qurban', 'kenan', null, undefined, ['sinema', 'kitab']];
+console.log("salam");
+console.log(1234);
+console.log(true);
+console.log([1, 2, 3]);
+console.error("sehv oldu");
+console.warn("xeberdarliq");
 
-let names1 = ['qurban', 'kenan', 'eli'];
-let years1 = ['1993', '2022', '1995'];
-let mix1 = ['qurban', 'kenan', null, undefined, ['sinema', 'kitab']];
+// rey bildirmek
 
-//find
-function over18(year) {
-    let age = 2022 - year;
-    return age >= 18;
-}
-//console.log(over18(2016));
-let val1 = years.find(over18);
-console.log(val1)
+/*
+dkjflksadf
+dskf;dkjflksadflkfjl;
+dhflksd
+*/
 
-//filter
-let val2 = years.filter(over18);
-console.log(val2)
-
-
-//splice
-console.log(names1);
-names1.splice(1, 0, 'ehmed')
-console.log(names1);
-
-//concat
-let val = years.concat(names);
-console.log(val);
-
-//sort
-years.sort();
-
-//reverse
-names.reverse();
-
-//indexof
-let index = names.indexOf('eli');
-console.log('index ' + index)
-
-//remove item
-years.pop();
-years.shift();
-
-//add item
-years.push('1996');
-years.unshift('1997');
-
-//set array item
-//names[5] = 'veli';
-names[names.length] = 'veli';
-
-// get array item
-console.log(names[1]);
-console.log(mix[4]);
-
-console.log(names);
-console.log(names.length);
-console.log(typeof names);
-
-console.log(years);
-console.log(mix);
+console.clear();
