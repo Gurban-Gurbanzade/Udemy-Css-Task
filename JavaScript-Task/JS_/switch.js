5956ff628e03b301c52014bfe05636d56094d73d
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-23 18:38:05 +0400

    Switch Example

---
+++ JavaScript-Task/JS_/switch.js
@@ -0,0 +1,39 @@
+// let category = "com";
+
+// switch (category) {
+//   case "telefon":
+//     console.log("telefon katqoriyasi");
+//     break;
+//   case "comp":
+//     console.log("komputer katqoriyasi");
+//     break;
+//   default:
+//     console.log("sehv katqoriya");
+// }
+
+// let day;
+// switch (new Date().getDay()) {
+//   case 0:
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     day = "hefte ici";
+//     break;
+//   case 5:
+//   case 6:
+//     day = "hefte sonu";
+//     break;
+// }
+
+// console.log(`bu gun ${day} gunudur`);
+
+const age = 15;
+switch (true) {
+  case age > 18:
+    console.log("kenan yeniyetmedir");
+    break;
+  case age < 18:
+    console.log("kenan usaqdir");
+    break;
+}
