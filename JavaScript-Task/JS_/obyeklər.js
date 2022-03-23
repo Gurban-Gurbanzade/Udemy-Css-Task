15916be9022d03a51c18d582091c1bc811b50dc3
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-23 22:53:57 +0400

    Object Example

---
+++ JavaScript-Task/JS_/obyeklər.js
@@ -0,0 +1,126 @@
+let avtoInfo = [
+  {
+    id: "bmw116d_123",
+    model: "bmw 116d",
+    il: 2015,
+    reng: "ag",
+    servisInfo: [
+      {
+        id: "bmw116d_123_1",
+        tarix: "20.01.2016",
+        km: "13000",
+        price: 900,
+        detay: [
+          {
+            id: "bmw116d_123_1_1",
+            aciqlama: "yag deyisme",
+            price: 300,
+          },
+          {
+            id: "bmw116d_123_1_2",
+            aciqlama: "filter deyisme",
+            price: 300,
+          },
+          {
+            id: "bmw116d_123_1_3",
+            aciqlama: "tormoz deyisme",
+            price: 300,
+          },
+        ],
+      },
+      {
+        id: "bmw116d_123_2",
+        tarix: "10.01.2017",
+        km: "28000",
+        price: 1800,
+        detay: [
+          {
+            id: "bmw116d_123_2_1",
+            aciqlama: "yag deyisme",
+            price: 350,
+          },
+          {
+            id: "bmw116d_123_2_2",
+            aciqlama: "filter deyisme",
+            price: 350,
+          },
+          {
+            id: "bmw116d_123_2_3",
+            aciqlama: "tormoz deyisme",
+            price: 800,
+          },
+          {
+            id: "bmw116d_123_2_4",
+            aciqlama: "bolt deyisme",
+            price: 300,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "bmw116d_123",
+    model: "bmw 116d",
+    il: 2015,
+    reng: "ag",
+    servisInfo: [
+      {
+        id: "bmw116d_123_1",
+        tarix: "20.01.2016",
+        km: "13000",
+        price: 900,
+        detay: [
+          {
+            id: "bmw116d_123_1_1",
+            aciqlama: "yag deyisme",
+            price: 300,
+          },
+          {
+            id: "bmw116d_123_1_2",
+            aciqlama: "filter deyisme",
+            price: 300,
+          },
+          {
+            id: "bmw116d_123_1_3",
+            aciqlama: "tormoz deyisme",
+            price: 300,
+          },
+        ],
+      },
+      {
+        id: "bmw116d_123_2",
+        tarix: "10.01.2017",
+        km: "28000",
+        price: 1800,
+        detay: [
+          {
+            id: "bmw116d_123_2_1",
+            aciqlama: "yag deyisme",
+            price: 350,
+          },
+          {
+            id: "bmw116d_123_2_2",
+            aciqlama: "filter deyisme",
+            price: 350,
+          },
+          {
+            id: "bmw116d_123_2_3",
+            aciqlama: "tormoz deyisme",
+            price: 800,
+          },
+          {
+            id: "bmw116d_123_2_4",
+            aciqlama: "bolt deyisme",
+            price: 300,
+          },
+        ],
+      },
+    ],
+  },
+];
+console.log(avtoInfo[0]);
+console.log(avtoInfo[0].id);
+console.log(avtoInfo[0].model);
+console.log(avtoInfo[0].servisInfo);
+console.log(avtoInfo[0].servisInfo[0]);
+console.log(avtoInfo[0].servisInfo[0].detay);
